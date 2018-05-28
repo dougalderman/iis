3e5ca970408c676588f1a3c5ef85cb74362b0819
Doug Alderman <agnadcp-d1@yahoo.com>
2018-05-28 13:37:01 -0600

    Quiz Templates endpoints working.

--- server/controllers/endpointsController.ts
+++ server/controllers/endpointsController.ts
@@ -9,5 +9,10 @@ export class EndpointsController {
     console.log('in runEndpoints()');
     // Quizzes
     app.post('/api/admin/quiz_templates', QuizTemplatesController.create) // Writes new template.
+    app.get('/api/admin/quiz_templates/id/:id', QuizTemplatesController.readById) // Reads template by id.
+    app.get('/api/admin/quiz_templates/name/:name', QuizTemplatesController.readByName) // Reads template by name.
+    app.get('/api/admin/quiz_templates', QuizTemplatesController.readAll) // Reads all templates.
+    app.put('/api/admin/quiz_templates/:id', QuizTemplatesController.update) // Updates template.
+    app.delete('/api/admin/quiz_templates/:id', QuizTemplatesController.delete) // Deletes template.
   }
 }
--- server/controllers/quizzes/quizTemplatesController.ts
+++ server/controllers/quizzes/quizTemplatesController.ts
@@ -9,13 +9,13 @@ class Template implements QuizTemplates {
   constructor(reqName: string, reqDescription: string) {
     this.name = reqName;
     this.description = reqDescription;
-  }
+  };
 }
 
 export class QuizTemplatesController {
 
   static create(req, res) : void {
-    console.log('in QuizTemplatesController');
+    console.log('in QuizTemplatesController--create()');
     console.log('req.body: ', req.body);
     if (req.body) {
       const pgSqlPool = new Pool();
@@ -23,7 +23,7 @@ export class QuizTemplatesController {
       const query = {
         text: 'INSERT INTO QuizTemplates(name, description) VALUES($1, $2)',
         values: [template.name, template.description]
-      }
+      };
       console.log('query: ', query);
       pgSqlPool.query(query)
       .then(result => {
@@ -37,6 +37,149 @@ export class QuizTemplatesController {
         console.error(e.stack);
         return res.status(500).send(e);
       });
+    }
+    else {
+      return res.status(500).send('invalid request');
+    }
+  }
+
+  static readById(req, res) : void {
+    console.log('in QuizTemplatesController--readById()');
+    console.log('req.params: ', req.params);
+    if (req.params && req.params.id) {
+      const pgSqlPool = new Pool();
+      const id = req.params.id;
+      const query = {
+        text: 'SELECT * FROM QuizTemplates WHERE ID = $1',
+        values: [id]
+      };
+      console.log('query: ', query);
+      pgSqlPool.query(query)
+      .then(result => {
+        console.log('result: ', result);
+        if (result) {
+          res.send(result);
+        }
+      })
+      .catch(e => {
+        console.error('in error');
+        console.error(e.stack);
+        return res.status(500).send(e);
+      });
+    }
+    else {
+      return res.status(500).send('invalid request');
+    }
+  }
+
+  static readByName(req, res) : void {
+    console.log('in QuizTemplatesController--readByName()');
+    console.log('req.params: ', req.params);
+    if (req.params && req.params.name) {
+      const pgSqlPool = new Pool();
+      const name = '%' + req.params.name + '%';
+      const query = {
+        text: 'SELECT * FROM QuizTemplates WHERE NAME ILIKE $1 ORDER BY NAME',
+        values: [name]
+      };
+      console.log('query: ', query);
+      pgSqlPool.query(query)
+      .then(result => {
+        console.log('result: ', result);
+        if (result) {
+          res.send(result);
+        }
+      })
+      .catch(e => {
+        console.error('in error');
+        console.error(e.stack);
+        return res.status(500).send(e);
+      });
+    }
+    else {
+      return res.status(500).send('invalid request');
+    }
+  }
+
+  static readAll(req, res) : void {
+    console.log('in QuizTemplatesController--readAll()');
+    const pgSqlPool = new Pool();
+    const query = {
+      text: 'SELECT * FROM QuizTemplates ORDER BY NAME',
+      values: []
+    };
+    console.log('query: ', query);
+    pgSqlPool.query(query)
+    .then(result => {
+      console.log('result: ', result);
+      if (result) {
+        res.send(result);
+      }
+    })
+    .catch(e => {
+      console.error('in error');
+      console.error(e.stack);
+      return res.status(500).send(e);
+    });
+  }
+
+  static update(req, res) : void {
+    console.log('in QuizTemplatesController--update()');
+    console.log('req.body: ', req.body);
+    console.log('req.params: ', req.params);
+    if (req.body && req.params && req.params.id) {
+      const pgSqlPool = new Pool();
+      const template = new Template(req.body.name, req.body.description);
+      const id = req.params.id;
+      const query = {
+        text: 'UPDATE QuizTemplates SET NAME = $1, DESCRIPTION = $2 WHERE ID = $3',
+        values: [template.name, template.description, id]
+      };
+      console.log('query: ', query);
+      pgSqlPool.query(query)
+      .then(result => {
+        console.log('result: ', result);
+        if (result) {
+          res.send(result);
+        }
+      })
+      .catch(e => {
+        console.error('in error');
+        console.error(e.stack);
+        return res.status(500).send(e);
+      });
+    }
+    else {
+      return res.status(500).send('invalid request');
+    }
+  }
+
+  static delete(req, res) : void {
+    console.log('in QuizTemplatesController--update()');
+    console.log('req.params: ', req.params);
+    if (req.params && req.params.id) {
+      const pgSqlPool = new Pool();
+      const id = req.params.id;
+      const query = {
+        text: 'DELETE FROM QuizTemplates WHERE ID = $1',
+        values: [id]
+      };
+      console.log('query: ', query);
+      pgSqlPool.query(query)
+      .then(result => {
+        console.log('result: ', result);
+        if (result) {
+          res.send(result);
+        }
+      })
+      .catch(e => {
+        console.error('in error');
+        console.error(e.stack);
+        return res.status(500).send(e);
+      });
+    }
+    else {
+      return res.status(500).send('invalid request');
     }
   }
 }
