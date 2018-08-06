d949c635a7f5d2b29d0d266688d554d08395079a
Doug Alderman <agnadcp-d1@yahoo.com>
2018-08-05 18:54:32 -0600

    Routing work in progress.

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FibroArticleComponent } from './fibro-article/fibro-article.component';
+import { AdminComponent } from './admin/admin/admin.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+
+
+const appRoutes: Routes = [
+  { path: 'fibro-article', component: FibroArticleComponent },
+  {
+    path: 'admin',
+    component: AdminComponent
+  },
+  { path: '',
+    redirectTo: '/fibro-article',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    FibroArticleComponent
+    FibroArticleComponent,
+    AdminComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
