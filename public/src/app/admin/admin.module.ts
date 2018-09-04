d9a5c2d2cb1fc0d2b4a4db7bc884f4d6b44ee6d1
Doug Alderman <agnadcp-d1@yahoo.com>
2018-09-03 18:25:36 -0600

    Create modify template form work in progress.

--- public/src/app/admin/admin.module.ts
+++ public/src/app/admin/admin.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { CommonModule }   from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AdminComponent } from './admin.component';
 import { AdminRoutingModule } from './admin-routing.module';
@@ -10,6 +12,8 @@ import { ActivateSurveyTemplateComponent } from './activate-survey-template/acti
 
 @NgModule({
   imports: [
+    CommonModule,
+    FormsModule,
     AdminRoutingModule
   ],
   declarations: [
--- public/src/app/admin/create-modify-quiz-template/create-modify-quiz-template.component.ts
+++ public/src/app/admin/create-modify-quiz-template/create-modify-quiz-template.component.ts
@@ -12,4 +12,6 @@ export class CreateModifyQuizTemplateComponent implements OnInit {
   ngOnInit() {
   }
 
+  templates = []
+
 }
