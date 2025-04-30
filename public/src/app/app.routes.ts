03cc50be155af165857979dd162d705a3f5badb4
Doug Alderman <agnadcp-d1@yahoo.com>
2025-04-30 17:35:11 -0400

    Made home demo-menu route lazy loading.

--- public/src/app/app.routes.ts
+++ public/src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 
-import { DemoMenuComponent } from './components/demo-menu/demo-menu.component';
-
 export const appRoutes: Routes = [
-  { path: 'demo-menu', component: DemoMenuComponent },
+  { path: 'demo-menu', loadComponent: () => import('./components/demo-menu/demo-menu.component').then(mod => mod.DemoMenuComponent)},
   { path: 'fibro-article', loadComponent: () => import('./components/fibro-article/fibro-article.component').then(mod => mod.FibroArticleComponent), data: {title: 'Fibromyalgia - Wikipedia'}},
   { path: 'sample-page-one', loadComponent: () => import('./components/sample-page-one/sample-page-one.component').then(mod => mod.SamplePageOneComponent), data: {title: 'Sample Page One'}},
   { path: 'sample-page-two', loadComponent: () => import('./components/sample-page-two/sample-page-two.component').then(mod => mod.SamplePageTwoComponent), data: {title: 'Sample Page Two'}},
