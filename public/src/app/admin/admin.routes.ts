b859ea94c42a306ef5dea9a0df7065a9ef489523
Doug Alderman <agnadcp-d1@yahoo.com>
2025-05-18 20:36:13 -0400

    Added lazy loading to admin routes. Added spacing in app routes. Added line about lazy loading in README.

--- public/src/app/admin/admin.routes.ts
+++ public/src/app/admin/admin.routes.ts
@@ -5,20 +5,8 @@ import { CreateModifySurveyTemplateComponent } from './create-modify-survey-temp
 import { ActivateQuizSurveyTemplateComponent } from './activate-quiz-survey-template/activate-quiz-survey-template.component'
 
 export const adminRoutes: Route[] = [
-  {
-    path: 'create-modify-quiz-template',
-    component: CreateModifyQuizTemplateComponent
-  },
-  {
-    path: 'create-modify-survey-template',
-    component: CreateModifySurveyTemplateComponent
-  },
-  {
-    path: 'activate-quiz-survey-template',
-    component: ActivateQuizSurveyTemplateComponent
-  },
-  {
-    path: '',
-    component: AdminHomeComponent
-  },
+  { path: 'create-modify-quiz-template', loadComponent: () => import('./create-modify-quiz-template/create-modify-quiz-template.component').then(mod => mod.CreateModifyQuizTemplateComponent) },
+  { path: 'create-modify-survey-template', loadComponent: () => import('./create-modify-survey-template/create-modify-survey-template.component').then(mod => mod.CreateModifySurveyTemplateComponent) },
+  { path: 'activate-quiz-survey-template', loadComponent: () => import('./activate-quiz-survey-template/activate-quiz-survey-template.component').then(mod => mod.ActivateQuizSurveyTemplateComponent) },
+  { path: '', loadComponent: () => import('./admin-home/admin-home.component').then(mod => mod.AdminHomeComponent) },
 ];
--- public/src/app/app.routes.ts
+++ public/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
-  { path: 'demo-menu', loadComponent: () => import('./components/demo-menu/demo-menu.component').then(mod => mod.DemoMenuComponent)},
-  { path: 'fibro-article', loadComponent: () => import('./components/fibro-article/fibro-article.component').then(mod => mod.FibroArticleComponent), data: {title: 'Fibromyalgia - Wikipedia'}},
-  { path: 'sample-page-one', loadComponent: () => import('./components/sample-page-one/sample-page-one.component').then(mod => mod.SamplePageOneComponent), data: {title: 'Sample Page One'}},
-  { path: 'sample-page-two', loadComponent: () => import('./components/sample-page-two/sample-page-two.component').then(mod => mod.SamplePageTwoComponent), data: {title: 'Sample Page Two'}},
-  { path: 'take-quiz', loadComponent: () => import('./components/take-quiz/take-quiz.component').then(mod => mod.TakeQuizComponent)},
-  { path: 'take-survey', loadComponent: () => import('./components/take-survey/take-survey.component').then(mod => mod.TakeSurveyComponent)},
-  { path: 'admin', loadChildren: () => import('./admin/admin.routes').then(mod => mod.adminRoutes)},
+  { path: 'demo-menu', loadComponent: () => import('./components/demo-menu/demo-menu.component').then(mod => mod.DemoMenuComponent) },
+  { path: 'fibro-article', loadComponent: () => import('./components/fibro-article/fibro-article.component').then(mod => mod.FibroArticleComponent), data: {title: 'Fibromyalgia - Wikipedia'} },
+  { path: 'sample-page-one', loadComponent: () => import('./components/sample-page-one/sample-page-one.component').then(mod => mod.SamplePageOneComponent), data: {title: 'Sample Page One'} },
+  { path: 'sample-page-two', loadComponent: () => import('./components/sample-page-two/sample-page-two.component').then(mod => mod.SamplePageTwoComponent), data: {title: 'Sample Page Two'} },
+  { path: 'take-quiz', loadComponent: () => import('./components/take-quiz/take-quiz.component').then(mod => mod.TakeQuizComponent) },
+  { path: 'take-survey', loadComponent: () => import('./components/take-survey/take-survey.component').then(mod => mod.TakeSurveyComponent) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.routes').then(mod => mod.adminRoutes) },
   { path: '', redirectTo: '/demo-menu', pathMatch: 'full' },
-  { path: '**', loadComponent: () => import('./components/page-not-found/page-not-found.component').then(mod => mod.PageNotFoundComponent)},
+  { path: '**', loadComponent: () => import('./components/page-not-found/page-not-found.component').then(mod => mod.PageNotFoundComponent) },
 ];
